22fc87aca359861ab7f79d2776bb4dc6d2d86049
Scott Morris <scott@rockset.com>
2020-09-01 18:10:20 -0700

    basic QL app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as axios from 'axios';
+import rocksetConfigure from "@rockset/client";
+
+const ROCKSET_APIKEY = '<insert key>';
+const rockset = rocksetConfigure(ROCKSET_APIKEY, "https://api.rs2.usw2.rockset.com");
+ 
+
+
+
+
 
 function App() {
+  const [userId, setUserId] = React.useState('');
+  const [genre, setGenre] = React.useState('');
+  const [results, setResults] = React.useState([]);
+
+  const submit = () => {
+    rockset.queryLambdas.executeQueryLambdaByTag(
+      'commons',
+      'fetchMovies',
+      'dev',
+      {
+        "parameters": [
+          {
+            "name": "genre",
+            "type": "string",
+            "value": genre,
+          },
+          {
+            "name": "userId",
+            "type": "string",
+            "value": userId,
+          }
+        ]
+      },
+    )
+    .then(response => setResults(response.results))
+    .catch(error => console.error(error));;
+    axios.post(
+      "https://api.rs2.usw2.rockset.com/v1/orgs/self/ws/commons/lambdas/fetchMovies/tags/dev",
+      {
+        "parameters": [
+          {
+            "name": "genre",
+            "type": "string",
+            "value": genre,
+          },
+          {
+            "name": "userId",
+            "type": "string",
+            "value": userId,
+          }
+        ]
+      },  
+      {
+        "headers": {
+          "Authorization": `ApiKey ${ROCKSET_APIKEY}`
+        }
+      }
+    )
+    .then(response => setResults(response.data.results))
+    .catch(error => console.error(error));
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ paddingTop: 100 }}>
+        <div>
+          <p>User ID</p>
+          <input type="text" value={userId} onChange={(evt) => setUserId(evt.target.value)} />
+          <p>Genre</p>
+          <input type="text" value={genre} onChange={(evt) => setGenre(evt.target.value)} />
+        </div>
+        <br /><br />
+        <button onClick={submit}>Submit</button>
+        <div><p>Results</p>
+        {results.map(r => <div>{r.title}</div>)}
+        </div>
+      </div>
     </div>
   );
 }
